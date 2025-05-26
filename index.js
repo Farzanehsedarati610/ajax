1654594622c70c8b37e44cc26c88951daf8feecc
Farzanehsedarati610 <farzanehsedarati6@gmail.com>
2025-05-26 21:41:28 +0000

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(express.json()); // 🚨 Ensure JSON support!
 
 app.post("/transfer", (req, res) => {
     res.json({ success: true, message: "Transaction received." });
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
