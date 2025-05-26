3834c84c56c361f1edbf6515056bd7c81b276058
Farzanehsedarati610 <farzanehsedarati6@gmail.com>
2025-05-26 21:32:15 +0000

    server.js

--- server.js
+++ server.js
@@ -29,3 +29,13 @@ app.post("/transfer", (req, res) => {
     res.json({ success: true, message: "Transaction received." });
 });
 
+const express = require("express");
+const app = express();
+
+app.post("/transfer", (req, res) => {
+    res.json({ success: true, message: "Transaction received." });
+});
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
