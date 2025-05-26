747eee873a7d663924489f22d11103a9a938743b
Farzanehsedarati610 <farzanehsedarati6@gmail.com>
2025-05-26 21:56:27 +0000

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,3 @@ const app = express();
 const PORT = process.env.PORT || 80;  // ✅ Change from 10000 to 80
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const transferRoutes = require("./routes/transfer");
-app.use("/transfer", transferRoutes);
-
